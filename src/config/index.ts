7336e2aa7e23d3d09aaf0ef7f30fda745c9d5e50
Narendra Singh Jethi <narendrajethi220@gmail.com>
2026-01-08 13:15:36 +0530

    dockerize application and fix redis configuration

--- src/config/index.ts
+++ src/config/index.ts
@@ -18,7 +18,7 @@ export const serverConfig: ServerConfig = {
   PORT: Number(process.env.PORT) || 3000,
   MONGO_URI:process.env.MONGO_URI || "mongodb://localhost:27017/short_url",
   // REDIS_PORT:Number(process.env.REDIS_PORT) || 6379,
-  REDIS_URL: process.env.REDIS_HOST || "redis://localhost:6379",
+  REDIS_URL: process.env.REDIS_URL || "redis://localhost:6379",
   REDIS_COUNTER_KEY: process.env.REDIS_COUNTER_KEY || "url_shortener_counter_key",
   BASE_URL: process.env.BASE_URL || "http://localhost:3000"
 };
